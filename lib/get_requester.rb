99382036a665b912fc176b2c2be8e1f8d4bcd2fd
Nicholas Silas <njsilas@gmail.com>
2020-11-09 17:02:39 -0500

    commit tree acff48e6c94462e2535e6f7ddac27e40e5e84714

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    def initialize(website)
+    end
+  
+    
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+      end
+      def parse_json
+        JSON.parse(get_response_body)
+        end
+end
+
